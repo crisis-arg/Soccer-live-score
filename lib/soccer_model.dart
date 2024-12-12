79cd5e4a15c6baaac92191c2b6c963c4af6c8c72
crisis-argha <namespace.arg@gmail.com>
2024-12-12 21:42:06 +0530

    didn't handle if the match is draw(bool? winner)

--- lib/soccer_model.dart
+++ lib/soccer_model.dart
@@ -3,6 +3,7 @@ class SoccerMatch {
   Team home;
   Team away;
   Goal goal;
+
   SoccerMatch(this.fixture, this.home, this.away, this.goal);
 
   factory SoccerMatch.fromJson(Map<String, dynamic> json) {
@@ -15,12 +16,13 @@ class SoccerMatch {
   }
 }
 
-//store the fixture
 class Fixture {
   int id;
   String date;
   Status status;
+
   Fixture(this.id, this.date, this.status);
+
   factory Fixture.fromJson(Map<String, dynamic> json) {
     return Fixture(
       json['id'],
@@ -30,26 +32,28 @@ class Fixture {
   }
 }
 
-//store the status
 class Status {
   int elapsed;
   String long;
+
   Status(this.elapsed, this.long);
+
   factory Status.fromJson(Map<String, dynamic> json) {
     return Status(
-      json['elapsed'],
-      json['long'],
+      json['elapsed'] ?? 0,
+      json['long'] ?? 'Unknown',
     );
   }
 }
 
-//store team data
 class Team {
   int id;
   String name;
   String logoUrl;
-  bool winner;
+  bool? winner;
+
   Team(this.id, this.name, this.logoUrl, this.winner);
+
   factory Team.fromJson(Map<String, dynamic> json) {
     return Team(
       json['id'],
@@ -60,11 +64,12 @@ class Team {
   }
 }
 
-//store the goal data
 class Goal {
   int home;
   int away;
+
   Goal(this.home, this.away);
+
   factory Goal.fromJson(Map<String, dynamic> json) {
     return Goal(
       json['home'],
